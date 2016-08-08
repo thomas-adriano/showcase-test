c5ad06f56b41b7eefbcaf1a1bd1120ae9a467fa9
felipexw <felipexw>
2016-08-08 13:36:17 -0300

    Add method get item's recommendations.

--- app/showcase.js
+++ app/showcase.js
@@ -5,6 +5,7 @@ const script = scripts[scripts.length - 1];
 const apiKey = script.getAttribute('data-api-key');
 const eventUrl = EVENT_SERVER_URL + '?accessKey=' + apiKey;
 
+
 window.recomendaMeta = {
     page: {
 
@@ -27,3 +28,24 @@ window.sendEvent = function(args) {
         dataType: "json"
     });
 };
+
+window.getRecommendations = function(args) {
+    return $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: PREDICTION_SERVER_URL,
+        success: function(response) {
+          console.log('success');
+          loadDataInsideDiv(response);
+        },
+        error: function(error) {
+            console.log('não foi possível realizar a chamada');
+        },
+        data: JSON.stringify(args),
+        dataType: "json"
+    });
+}
+
+function loadDataInsideDiv(data){
+  console.log(data);
+}

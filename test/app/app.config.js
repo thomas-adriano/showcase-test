96cd5ca9f79629af0e6042cfe68e2190f27a8391
thomas.adriano <thomas.adriano@senior.com.br>
2016-08-10 17:21:38 -0300

    Alterado estilos da navbar

--- test/app/app.config.js
+++ test/app/app.config.js
@@ -6,4 +6,6 @@ function appCfg(toastr) {
     toastr.options.closeButton = true;
     toastr.options.timeOut = 2000;
     toastr.options.extendedTimeOut = 4000;
+    toastr.options.positionClass = 'toast-bottom-right';
+    toastr.options.newestOnTop = true;
 }
